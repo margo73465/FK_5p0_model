5fa315f2ad1bb9b4e4c05bf9bb6010fe8119c854
margo73465 <margo.lee.smith@gmail.com>
2014-12-12 17:53:13 -0500

    now using my stim and APD calculation, REALLY REALLY close to fitting daniel's voltage trace perfectly -- have not yet checked APD curves

--- FK_model/rebuild.cpp
+++ FK_model/rebuild.cpp
@@ -10,7 +10,7 @@
 #define EPS 1e-18
 #define NX 100 //number of divisions in cable ("cells") (use ~100 for 1D, 2 for 0D)
 #define NEXCITE 10 //number of excitations from stimulus current (-1 for APPLY S1)
-#define TOTALTIME 2400
+#define TOTALTIME 3000
 #define DT 0.01
 #define DX 0.02
 #define SPACING_INDEX 10
@@ -18,6 +18,7 @@
 
 #define xstimdur 3.0
 #define xstimamp 30.0
+#define APD_rec_site 3
 
 double heav(double u);
 
@@ -33,7 +34,9 @@ double heav(double u) {
 int main() {
 
   double savedt = DT;
-  int numt = TOTALTIME/DT;
+  // int numt = TOTALTIME/DT;
+  long int n = 0;
+  double t = 0.0;
   int istimdur = round(xstimdur/DT);
   int savepoints = TOTALTIME/savedt;
   int i, j, k;
@@ -43,12 +46,15 @@ int main() {
   double preV1 = 0, preV2 = 0;
 
   int numS1 = 0; //count how many S1 stim was applied (have ~5 inbetween each S2 stim)
-  float S1time = 400./DT; //CL(cycle length) between S1 stim
+  float S1time = 400. / DT; //CL(cycle length) between S1 stim
   int lastStimTime = 0;
   int lastPeakTime = 0;
+  double stim_time = 400. / DT;
+  double u_max = -100.0, u_low = 0.04966, u_90;
+  double APD, APD_start;
 
   double dv, dw, dd, tso;
-  vector<double> u(NX), v(NX), w(NX), d(NX), tempu(NX + 1, 0.0);
+  vector<double> u(NX), uP(NX), v(NX), w(NX), d(NX), tempu(NX + 1, 0.0);
   vector<double> xfi(NX), xso(NX), xsi(NX), xstim(NX);
   vector<double> para(NDIM);
   vector<double> excitimes(NEXCITE), nexcs(NEXCITE);
@@ -69,7 +75,7 @@ int main() {
   double xk, td, to, tsoa, tsob, uso, xtso, tsi, D, tvmm;
 
   ifstream input_file("xfinal.dat", ios::in);
-  ofstream output_file("rebuild_voltage.dat", ios::out);
+  ofstream output_file("rebuild_voltage_myStim.dat", ios::out);
 
   if(!input_file || !output_file) {
     cerr << "open error!!" << endl;
@@ -116,41 +122,41 @@ int main() {
   D = para[19]; //related to density, mostly changes CV, but can effect everything
   tvmm = para[20]; //controls the steepness of the CV curve (chaos 8)
 
-  for(i = 0; i < numt; i++) {
+  while (t < TOTALTIME) {
 
     for(j = 0; j < NX; j++) {
 
-      if (i >= S1time + lastStimTime && i <= S1time + lastStimTime + istimdur && j < 5 && numS1 < S1limit) {
-        if (j==4 && i + 1 > S1time + istimdur) {
-          numS1++;
-          lastStimTime = i;
-          printf("S1 @ %i\n", i);
-        }
-        xstim[j] = xstimamp;
-      }
-      else if (i >= nexcs[ni] && i <= nexcs[ni] + istimdur && j < 5) {
-        if (numS1 >= S1limit) {
-          if (j == 4 && i + 1 > nexcs[ni] + istimdur) {
-            ni++;
-            numS1 = 0;
-            lastStimTime = i;
-          }
-          xstim[j] = xstimamp;   
-        }
-      }
-      else {
-        xstim[j] = 0.0;
-      }
+      // if (i >= S1time + lastStimTime && i <= S1time + lastStimTime + istimdur && j < 5 && numS1 < S1limit) {
+      //   if (j==4 && i + 1 > S1time + istimdur) {
+      //     numS1++;
+      //     lastStimTime = i;
+      //     printf("S1 @ %i\n", i);
+      //   }
+      //   xstim[j] = xstimamp;
+      // }
+      // else if (i >= nexcs[ni] && i <= nexcs[ni] + istimdur && j < 5) {
+      //   if (numS1 >= S1limit) {
+      //     if (j == 4 && i + 1 > nexcs[ni] + istimdur) {
+      //       ni++;
+      //       numS1 = 0;
+      //       lastStimTime = i;
+      //     }
+      //     xstim[j] = xstimamp;   
+      //   }
+      // }
+      // else {
+      //   xstim[j] = 0.0;
+      // }
 
       // Stimlulus Current
-      // if(i == stim_time){
-      //   numS1++;
-      //   printf("Stim at time %i\n", i);
-      // }
-      // if(i - stim_time <= istimdur && i - stim_time > 0 && j < 5)
-      //   xstim[j] = xstimamp;
-      // else 
-      //   xstim[j] = xstimamp;
+      if (n == stim_time && j == 0) {
+        numS1++;
+        cout << "STIM: n = " << n << " stim_time = " << stim_time << endl;
+      }
+      if (n - stim_time <= istimdur && n - stim_time > 0 && j < 5)
+        xstim[j] = xstimamp;
+      else 
+        xstim[j] = 0.0;
 
       // Model Functions
       tso = tsoa + (tsob - tsoa) * (1 + tanh((u[j] - uso) * xtso))/2.0;
@@ -174,7 +180,7 @@ int main() {
       tempu[k] = u[k];
     }
 
-    for(k = 0; k < NX; k++) {
+    for (k = 0; k < NX; k++) {
 
       if (k != 0 && k != NX - 1) {
         u[k] = tempu[k] + DT * (D * (tempu[k+1] + tempu[k-1] - 2*tempu[k])/pow(DX,2.0) - (xfi[k] + xso[k] + xsi[k] - xstim[k]));
@@ -192,40 +198,50 @@ int main() {
     }
 
     //APD_90
-    if (i == stim_time - 1){
+    if (n == stim_time - 1) {
       //V_low = V[APD_rec_site];  
-      V_max = -100.0;
+      u_max = -1.0;
       //actual_DI = t - APD_end;
       //printf("DI = %d, APD reset, t = %f\n", DI[i], t);
     }
-    if (u[APD_rec_site] > V_max) {
-      V_max = u[APD_rec_site];
-      V_90 = V_max - (V_max - V_low)*0.90;
+    if (u[APD_rec_site] > u_max) {
+      u_max = u[APD_rec_site];
+      u_90 = u_max - (u_max - u_low)*0.90;
       APD_start = t;
+      // cout << "V_max = " << u_max << "V_90 = " << u_90 << endl;
       //printf("APD_start = %f \t V_max = %f \t V_90 = %f\n",APD_start,V_max,V_90);
     }
     //if (V[APD_rec_site] <= V_90 && VP[APD_rec_site] > V_90 && n - stim_time > 50){
     //double baseline = 85.7 * 0.1 - 84.0;
-    double baseline = V_90;
-    if (V[APD_rec_site] <= baseline && VP[APD_rec_site] > baseline && n - stim_time > 50) {
-      if(N < paceAP)
-        stim_time = (N + 1) * BCL / DT;
-      else if(N == paceAP){
-        stim_time = n + N_DI; 
+    // double baseline = V_90;
+    if (u[APD_rec_site] <= u_90 && uP[APD_rec_site] > u_90 && n - stim_time > 50) {
+      if (numS1 < S1limit)
+        stim_time = (numS1 + 1) * S1time;
+      else if (numS1 == S1limit) {
+        stim_time = n + 20000; 
       }
       APD = t - APD_start;
-      APD_end = t;
-      count++;
+      // APD_end = t;
+      // count++;
       printf("APD = %f\t time = %f\n", APD, t);
     }
 
+    for (k = 0; k < NX; k++) {
+      uP[k] = u[k];
+    }
+
     // Output
-    if (i % 10 == 0) {
+    if (n % 10 == 0) {
+      // cout << "t = " << t << " u[1] = " << u[1] << endl;
+      // cout << "time is " << t << endl;
       for (k = 0; k < NX; k++) {
         output_file << u[k] << " ";
       }
       output_file << endl;
     }
+
+    n++; 
+    t = n * DT;
 
   } // End of time loop
 }
--- FK_model/test.cpp
+++ FK_model/test.cpp
@@ -14,6 +14,13 @@ int main() {
     cout << test[i] << endl;
   }
 
+  double stim_time = 1000.03;
+  int n = 1000;
+
+  if (n == stim_time) {
+    cout << "HELLO" << endl;
+  }
+
 
   return 0;
 }
