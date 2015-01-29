58eca81b6b673219d69a91db56b1105d3f59bacb
Margo73465 <margo.lee.smith@gmail.com>
2015-01-29 15:33:59 -0500

    restitution GA working, adjusted APD measurement, still need to set it up for command line parameters and saving output

--- FK_model/FK4V_5p0_cell_GA_restitution.cpp
+++ FK_model/FK4V_5p0_cell_GA_restitution.cpp
@@ -8,11 +8,11 @@
 
 #define NDIM 21 //number of parameters, used to be 23
 #define DT 0.01
-#define S1limit 5
-#define BCL 400.
+#define S1limit 10
+#define BCL 500.
 
-#define xstimdur 3.0
-#define xstimamp 3
+#define stim_dur 3.0
+#define stim_amp 3
 
 double heav(double u);
 
@@ -29,19 +29,19 @@ int main(int argc, char *argv[]) {
 
   // Simulation parameters
   long int n;
-  double t;
-  int istimdur = round(xstimdur/DT);
-  int i, j, k;
+  double t, stim_time;
+  int N_stim = round(stim_dur / DT);
+  int N_BCL = round(BCL / DT);
+  int numS1 = 0; 
   double TOTALTIME = S1limit * BCL + 1000.0;
-  
-  int numS1 = 0; //count how many S1 stim was applied (have ~5 inbetween each S2 stim)
-  float S1time = BCL / DT; //CL(cycle length) between S1 stim
-  double stim_time = BCL / DT;
 
-  double dv, dw, dd, tso;
+  // State variables
+  double du, dv, dw, dd, tso;
   double u = 0.0, uP = 0.0, v = 1.0, w = 1.0, d = 0.0, tempu;
   double xfi, xso, xsi, xstim;
-  double para(NDIM);
+
+  // Parameter vector
+  vector<double> para(NDIM);
   
   //----------------KNOW PARAMETERS------------------//
   double uo, um, una;
@@ -50,22 +50,21 @@ int main(int argc, char *argv[]) {
   double xk, td, to, tsoa, tsob, uso, xtso, tsi, D, tvmm;
 
   ifstream input_file("xfinal.dat", ios::in);
-  ofstream output_voltage("rebuild_voltage_myStim.dat", ios::out);
-  ofstream output_APD("rebuild_APD_restitution_cell70.dat", ios::out);
+  ofstream output_APD("FK4V_5p0_cell_GA_restitution_danParams.dat", ios::out);
 
   // Read parameters from file
-  if(!input_file || !output_voltage || !output_APD) {
+  if(!input_file || !output_APD) {
     cerr << "open error!!" << endl;
     exit(1);
   }
-  for(i = 0; i < NDIM; i++) {
+  for(int i = 0; i < NDIM; i++) {
     input_file >> para[i];
   }
   input_file.close();
   
   // Restitution protocol parameters
-  // int DI[] = {200, 180, 160, 140, 120, 100, 90, 80, 70, 60, 50, 40, 30, 25, 20, 15, 10, 5};
-  int DI[] = {254.0690, 222.4222, 182.5915, 152.8019, 123.0218, 103.2425, 83.4569, 43.7630, 24.1447, 14.3405};
+  int DI[] = {200, 180, 160, 140, 120, 100, 90, 80, 70, 60, 50, 40, 30, 25, 20, 15, 10, 5};
+  // double DI[] = {254.0690, 222.4222, 182.5915, 152.8019, 123.0218, 103.2425, 83.4569, 43.7630, 24.1447, 14.3405};
   int N_DI;
   int size = sizeof(DI)/sizeof(DI[0]);
 
@@ -97,50 +96,51 @@ int main(int argc, char *argv[]) {
   tvmm = para[20]; //controls the steepness of the CV curve (chaos 8)
   
   // Restitution protocol loop
-  for(i = 0; i < size; i++) {
+  for(int i = 0; i < size; i++) {
 
     t = 0.0;
     n = 0;
     numS1 = 0;
     N_DI = DI[i] / DT;
-    stim_time = S1time;
-    // t_end = S1limit * 400.0 + 1000.0;
+    stim_time = 0;
 
     // APD parameters
-    double u_max = -1.0, u_low = 0.04966, u_90, obj_APD;
-    double APD, APD_start;
-    double u_max_2 = -1.0, u_low_2 = 0.04966, u_90_2;
-    double APD_2, APD_start_2;
-
+    // double u_max = -1.0, u_low = 0.04966, 
+    double u_max = -1.0, u_low = 0.0;
+    double u_90, obj_APD;
+    double APD, APD_start = 0.0;
 
     while (t < TOTALTIME) {
 
       // Stimlulus Current
-      if (n == stim_time && j == 0) {
+      if (n == stim_time) {
         numS1++;
-        cout << "STIM: n = " << n << " stim_time = " << stim_time << endl;
+        // cout << "STIM: n = " << n << " stim_time = " << stim_time << endl;
       }
-      if (n - stim_time <= istimdur && n - stim_time > 0 && j < 5)
-        xstim[j] = xstimamp;
+      if (n - stim_time <= N_stim && n - stim_time > 0)
+        xstim = stim_amp;
       else 
-        xstim[j] = 0.0;
+        xstim = 0.0;
 
       // Model Functions
-      tso = tsoa + (tsob - tsoa) * (1 + tanh((u[j] - uso) * xtso))/2.0;
+      tso = tsoa + (tsob - tsoa) * (1 + tanh((u - uso) * xtso))/2.0;
 
-      dv = (1 - heav(u[j] - una)) * (1 - v[j]) / ((1 - heav(u[j] - uv)) * tvm + tvmm * heav(u[j] - uv)) - heav(u[j] - una) * v[j] / tvp;
-      dw = (1 - heav(u[j] - uw)) * (1 - w[j]) / twm - heav(u[j] - uw) * w[j] / twp;
-      dd = ((1 - heav(u[j] - ud)) / tsm + heav(u[j] - ud) / tsp) * ((1 + tanh(xk * (u[j] - ucsi))) / 2.0 - d[j]);
+      dv = (1 - heav(u - una)) * (1 - v) / ((1 - heav(u - uv)) * tvm + tvmm * heav(u - uv)) - heav(u - una) * v / tvp;
+      dw = (1 - heav(u - uw)) * (1 - w) / twm - heav(u - uw) * w / twp;
+      dd = ((1 - heav(u - ud)) / tsm + heav(u - ud) / tsp) * ((1 + tanh(xk * (u - ucsi))) / 2.0 - d);
 
-      v[j] = v[j] + DT * dv;  //fast gate
-      w[j] = w[j] + DT * dw;  //slow gate
-      d[j] = d[j] + DT * dd;
+      v = v + DT * dv;  //fast gate
+      w = w + DT * dw;  //slow gate
+      d = d + DT * dd;
 
       // Currents
-      xfi[j] = -v[j] * heav(u[j] - una) * (u[j] - una) * (um - u[j]) / td;
-      xso[j] = (u[j] - uo) * (1 - heav(u[j] - uc)) / to + heav(u[j] - uc) / tso;
-      xsi[j] = -w[j] * d[j] / tsi;
+      xfi = -v * heav(u - una) * (u - una) * (um - u) / td;
+      xso = (u - uo) * (1 - heav(u - uc)) / to + heav(u - uc) / tso;
+      xsi = -w * d / tsi;
 
+      // du_dt
+      du = - (xfi + xso + xsi - xstim);
+      u = u + DT * du;
 
       // Determine time of next stimulus using APD_90
       if (n == stim_time - 1) {
@@ -149,13 +149,14 @@ int main(int argc, char *argv[]) {
       if (u > u_max) {
         u_max = u;
         u_90 = u_max - (u_max - u_low) * 0.90;
+        // u_90 = 0.1;
         APD_start = t;
       }
       if (u <= u_90 && uP > u_90 && n - stim_time > 50) {
         APD = t - APD_start;
-        cout << "time = " << t << " APD_2 = " << APD_2 << " u_90 = " << u_90 << endl;
+        cout << "time = " << t << " APD_start = " << APD_start << " APD = " << APD << " u_90 = " << u_90 << endl;
         if (numS1 < S1limit)
-          stim_time = (numS1 + 1) * S1time;
+          stim_time = (numS1 + 1) * N_BCL;
         else if (numS1 == S1limit) {
           stim_time = n + N_DI; 
           cout << "last stim at: " << stim_time << endl;
@@ -166,7 +167,7 @@ int main(int argc, char *argv[]) {
       
       // Output
       if (n % 10 == 0) {
-        output_voltage << u[k] << " ";
+        // output_voltage << u << " ";
       }
 
       n++; 
